1508a57312faec6188d668891f92915dcd6b5c63
TarraSai <tarrasai01@gmail.com>
2023-10-28 20:59:53 +0530

    commit sai

--- Crave_React/src/Restaurant_Interfaces_folder/Restaurant_Partnering_Landing_page.js
+++ Crave_React/src/Restaurant_Interfaces_folder/Restaurant_Partnering_Landing_page.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function RPLP() {
   return (
     <div>
-      Restaurant_Partnering_Landing_page
+      <h1>Restaurant_Partnering_Landing_page</h1>
     </div>
-  )
+  );
 }
